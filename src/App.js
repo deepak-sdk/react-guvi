ff06efa8af597d0bec47172c2455bea8df8ba530
deepak-sdk <itisdeepakkumar@outlook.com>
2021-11-01 17:01:00 +0530

    react-day-one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
+
+  const users = [
+    { name: "DK", pic: "https://cdn.onlinewebfonts.com/svg/img_24787.png" },
+    { name: "Got it", pic: "https://i.etsystatic.com/15971018/r/il/14c008/1957171418/il_794xN.1957171418_r3wl.jpg" }
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello React.</h1>
+
+      {/* passing value to the props */}
+
+      {/* <Msg name="Got It" />
+      <Msg name="DK" /> */}
+
+      {/* Transforming - Array of String to Array of JSX */}
+
+      {users.map(value =>
+        <Msg name={value.name} pic={value.pic} />)}
+
+
+
     </div>
   );
 }
 
-export default App;
+// Creating Components
+// First Letter Caps
+// return JSX
+
+function Msg({ name, pic }) {
+  // let name = "DK";
+  console.log({ name });
+
+  return (
+    <div>
+      <img className="user-pic" src={pic} />
+      <h2 className="user-msg">Lets Start - {name}</h2>
+    </div>
+  );
+}
+
+//App - component can created by
+//1. function component
+//2. class component
+
+// jsx -> javascript xml / javascript extented
+// domain-specific language called JSX.
+
+//noscript
+// If JS is disabled in browser, content inside noscript will be displayed
